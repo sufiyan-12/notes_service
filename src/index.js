e324eec80fc5ddf07daef291c253a67886ce5ae8
Mohd Sufiyan <sufiyan@timelybills.app>
2024-08-18 20:34:06 +0530

    update

--- src/index.js
+++ src/index.js
@@ -21,11 +21,23 @@ app.get("/", (req, res)=>{
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    app.listen(PORT, ()=>{
-    console.log("Server started on port no: 5000")
-    });
-}).catch((error)=>{
-    console.log(error);
-})
+if(process.env.MONGO_URL === undefined){
+    const url = "mongodb+srv://msufiyan576:sufiyan1234@cluster0.b6e1x.mongodb.net/notes_db?retryWrites=true&w=majority&appName=Cluster0"
+    mongoose.connect(url)
+    .then(()=>{
+        app.listen(PORT, ()=>{
+        console.log("Server started on port no: 5000")
+        });
+    }).catch((error)=>{
+        console.log(error);
+    })
+} else{
+    mongoose.connect(process.env.MONGO_URL)
+    .then(()=>{
+        app.listen(PORT, ()=>{
+        console.log("Server started on port no: 5000")
+        });
+    }).catch((error)=>{
+        console.log(error);
+    })
+}
