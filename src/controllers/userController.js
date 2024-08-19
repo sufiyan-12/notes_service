3470eae11cbd0fa5db0ddde7578a9fff70483e68
Mohd Sufiyan <sufiyan@timelybills.app>
2024-08-19 16:27:23 +0530

    bug fix in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const userModel = require("../models/user")
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.MONGO_URL
+const SECRET_KEY = process.env.SECRET_KEY
 
 const signup = async (req, res) => {
     // existing user check
